fd425ad2131aec9287fdaf8a787bbb9be2fc4767
Christoph Van Gestel <christoph.vangestel@gmail.com>
2015-09-06 16:42:25 +0200

    Introduction to programming with Python - Modules 1 and 2.

---
+++ MVA Introduction to prograaming with Python/DisplayText/DisplayText/DisplayText.py
@@ -0,0 +1,3 @@
+# Print some text in console
+print('The Capybara is the worlds largest rodent.')
+print('It can also swim.')
---
+++ MVA Introduction to prograaming with Python/HelloWorld/HelloWorld/HelloWorld.py
@@ -0,0 +1,5 @@
+# Helllo world application
+# 2015/09/06; cvg
+# Prints a message in console
+
+print( 'Hello world!')
